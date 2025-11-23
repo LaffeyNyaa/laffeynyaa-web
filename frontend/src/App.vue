3f5ad1d18b1ef6043a08ff11a9f4bfff14ae3e10
LaffeyNyaa <LaffeyNyaa@outlook.com>
2025-11-23 17:01:13 +0800

    feat: add footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import Button from './components/ButtonPrimary.vue'
 import Navbar from './components/NavbarTop.vue'
-import ContainerCenter from './components/ContainerCenter.vue'
+import ContainerCenter from './components/ContainerContent.vue'
+import FooterBottom from './components/FooterCopyright.vue'
 </script>
 
 <template>
@@ -13,6 +14,11 @@ import ContainerCenter from './components/ContainerCenter.vue'
     <ContainerCenter>
         <img src="./assets/images/miku-pay.gif" alt="" />
     </ContainerCenter>
+
+    <FooterBottom>
+        <p>Copyright (c) 2025 LaffeyNyaa</p>
+        <a href="https://beian.miit.gov.cn/" target="_blank">津ICP备2024026458号-1</a>
+    </FooterBottom>
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/FooterCopyright.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer>
+        <slot></slot>
+    </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@use '../assets/styles/variables.scss' as *;
+
+footer {
+    display: flex;
+    margin-top: $margin-size;
+    padding: $padding-size;
+    align-items: center;
+    flex-direction: column;
+    background-color: $theme-background-color;
+    border-radius: 10px;
+}
+</style>
